80a2f5756ff3ae47f277b929cad36519229dfc61
Håvard Skordal Thorsen <hstst2006@gmail.com>
2023-12-02 22:42:37 +0100

    Listify no longer uses Option<T>

--- src/lib.rs
+++ src/lib.rs
@@ -4,10 +4,14 @@ pub mod listify
     use std::path::Path;
 
     // Return a vector of the lines in a text file. Empty lines are None
-    pub fn listify<T: std::str::FromStr>(input_file: &Path) -> std::io::Result<Vec<Option<T>>> 
+    pub fn listify<T: std::str::FromStr>(input_file: &Path, delimiter: char) -> std::io::Result<Vec<T>>
     {
         let input_content = read_to_string(input_file)?;
-        let output_content: Vec<Option<T>> = input_content.lines().map(|line| { if line == "" { None } else { line.parse().ok()}}).collect();
+        
+        let output_content: Vec<T> = input_content.split(delimiter).filter_map(|row| row.parse().ok()).collect();
+
+        // Returns lines as Some(content), and empty lines as None
+        //let output_content: Vec<Option<T>> = input_content.split(delimiter).map(|row| { if row == "" { None } else { row.parse().ok()}}).collect();
 
         Ok(output_content)
     }
